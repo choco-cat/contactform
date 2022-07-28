0475213ef25e3ee3d1dbc4db154be59f85eb87ee
Natalia Panfilenka <choco-cat@mail.ru>
2022-07-28 10:19:18 +0300

    feat: validation all inputs by submit form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,95 +1,98 @@
 import React, { useState, ChangeEvent } from "react";
 import cl from "./form.module.scss";
 
+interface IFormData {
+  value: string;
+  error: string;
+}
+
 const UserForm: React.FC = () => {
-  const [userName, setUserName] = useState("");
-  const [userPhone, setUserPhone] = useState("+7");
-  const [errors, setError] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    birth: "",
-    message: "",
-  });
+  const [userName, setUserName] = useState({} as IFormData);
+  const [userEmail, setUserEmail] = useState({} as IFormData);
+  const [userPhone, setUserPhone] = useState({} as IFormData);
+  const [userBirth, setUserBirth] = useState({} as IFormData);
+  const [userMessage, setUserMessage] = useState({} as IFormData);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    return !Object.values(errors).find((value) => value !== "");
-    // TODO: run validation for all inputs - use refs?
-    // TODO: send ajax request to server
+    validationName(userName.value);
+    validationEmail(userEmail.value);
+    validationPhone(userPhone.value);
+    validationBirth(userBirth.value);
+    validationMessage(userMessage.value);
+    // TODO: calc all errors. if 0, then send ajax request to server
   };
 
   const transformName = (e: ChangeEvent<HTMLInputElement>) => {
-    setUserName(e.target.value.toUpperCase());
+    setUserName({ ...userName, value: e.target.value.toUpperCase() });
     // TODO: set cursor position after setUserName - use ref?
-    if (errors.name) {
-      validationName(e);
+    if (userName.error) {
+      validationName(e.target.value);
     }
   };
 
-  const validationName = (e: ChangeEvent<HTMLInputElement>) => {
-    setError({
-      ...errors,
-      name: !/^[A-Za-z]{3,30} [A-Za-z]{3,30}$/.test(e.target.value as string)
+  const validationName = (value: string) => {
+    setUserName({
+      ...userName,
+      error: !/^[A-Za-z]{3,30} [A-Za-z]{3,30}$/.test(value as string)
         ? "The field must contain two words from 3 to 30 letter characters long!"
         : "",
     });
   };
 
   const changeEmail = (e: ChangeEvent<HTMLInputElement>) => {
-    if (errors.email) {
-      validationEmail(e);
+    setUserEmail({ ...userEmail, value: e.target.value });
+    if (userEmail.error) {
+      validationEmail(e.target.value);
     }
   };
 
-  const validationEmail = (e: ChangeEvent<HTMLInputElement>) => {
-    setError({
-      ...errors,
-      email: !/^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,4}$/i.test(
-        e.target.value as string
-      )
+  const validationEmail = (value: string) => {
+    setUserEmail({
+      ...userEmail,
+      error: !/^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,4}$/i.test(value as string)
         ? "Wrong email!"
         : "",
     });
   };
 
   const transformPhone = (e: ChangeEvent<HTMLInputElement>) => {
-    setUserPhone(e.target.value);
-    if (errors.phone) {
-      validationPhone(e);
+    setUserPhone({ ...userPhone, value: e.target.value });
+    if (userPhone.error) {
+      validationPhone(e.target.value);
     }
   };
 
-  const validationPhone = (e: ChangeEvent<HTMLInputElement>) => {
-    setError({
-      ...errors,
-      phone: !/^\+7[\d]{10}$/.test(e.target.value as string)
-        ? "Wrong phone!"
-        : "",
+  const validationPhone = (value: string) => {
+    setUserPhone({
+      ...userPhone,
+      error: !/^\+7[\d]{10}$/.test(value as string) ? "Wrong phone!" : "",
     });
   };
 
   const changeBirth = (e: ChangeEvent<HTMLInputElement>) => {
-    if (errors.birth) {
-      validationBirth(e);
+    setUserBirth({ ...userBirth, value: e.target.value });
+    if (userBirth.error) {
+      validationBirth(e.target.value);
     }
   };
 
-  const validationBirth = (e: ChangeEvent<HTMLInputElement>) => {
-    setError({ ...errors, birth: !e.target.value.length ? "Wrong date!" : "" });
+  const validationBirth = (value: string) => {
+    setUserBirth({ ...userBirth, error: !value.length ? "Wrong date!" : "" });
   };
 
   const changeMessage = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    if (errors.message) {
-      validationMessage(e);
+    setUserMessage({ ...userMessage, value: e.target.value });
+    if (userMessage.error) {
+      validationMessage(e.target.value);
     }
   };
 
-  const validationMessage = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setError({
-      ...errors,
-      message:
-        e.target.value.length < 10 || e.target.value.length > 300
+  const validationMessage = (value: string) => {
+    setUserMessage({
+      ...userMessage,
+      error:
+        value.length < 10 || value.length > 300
           ? "message length must be greater than 3 and less than 300!"
           : "",
     });
@@ -103,14 +106,17 @@ const UserForm: React.FC = () => {
           <input
             className={cl.form__input}
             type="text"
-            value={userName}
+            value={userName.value}
             onChange={transformName}
-            onBlur={validationName}
-            required
+            onBlur={(e: ChangeEvent<HTMLInputElement>) =>
+              validationName(e.target.value)
+            }
             minLength={3}
             maxLength={35}
           />
-          {errors.name && <div className={cl.form__error}>{errors.name}</div>}
+          {userName.error && (
+            <div className={cl.form__error}>{userName.error}</div>
+          )}
         </label>
 
         <label className={cl.form__label}>
@@ -119,26 +125,33 @@ const UserForm: React.FC = () => {
             className={cl.form__input}
             type="text"
             onChange={changeEmail}
-            onBlur={validationEmail}
-            required
+            onBlur={(e: ChangeEvent<HTMLInputElement>) =>
+              validationEmail(e.target.value)
+            }
             minLength={3}
             maxLength={35}
           />
-          {errors.email && <div className={cl.form__error}>{errors.email}</div>}
+          {userEmail.error && (
+            <div className={cl.form__error}>{userEmail.error}</div>
+          )}
         </label>
         <label className={cl.form__label}>
           Phone
           <input
             className={cl.form__input}
             type="text"
-            value={userPhone}
+            placeholder="+7__________"
+            value={userPhone.value}
             onChange={transformPhone}
-            onBlur={validationPhone}
-            required
+            onBlur={(e: ChangeEvent<HTMLInputElement>) =>
+              validationPhone(e.target.value)
+            }
             minLength={12}
             maxLength={12}
           />
-          {errors.phone && <div className={cl.form__error}>{errors.phone}</div>}
+          {userPhone.error && (
+            <div className={cl.form__error}>{userPhone.error}</div>
+          )}
         </label>
         <label className={cl.form__label}>
           Birthday
@@ -146,25 +159,29 @@ const UserForm: React.FC = () => {
             className={cl.form__input}
             type="date"
             onChange={changeBirth}
-            onBlur={validationBirth}
-            required
+            onBlur={(e: ChangeEvent<HTMLInputElement>) =>
+              validationBirth(e.target.value)
+            }
             minLength={3}
             maxLength={15}
           />
-          {errors.birth && <div className={cl.form__error}>{errors.birth}</div>}
+          {userBirth.error && (
+            <div className={cl.form__error}>{userBirth.error}</div>
+          )}
         </label>
         <label className={cl.form__label}>
           Message
           <textarea
             className={cl.form__input}
             onChange={changeMessage}
-            onBlur={validationMessage}
-            required
+            onBlur={(e: ChangeEvent<HTMLTextAreaElement>) =>
+              validationMessage(e.target.value)
+            }
             minLength={10}
             maxLength={300}
           />
-          {errors.message && (
-            <div className={cl.form__error}>{errors.message}</div>
+          {userMessage.error && (
+            <div className={cl.form__error}>{userMessage.error}</div>
           )}
         </label>
         <input type="submit" value="Send" />
